4465bc14dfb145569d5c15a4253235cd99af5b03
Althea Smith <asmith790@ufl.edu>
2018-11-18 17:14:15 -0500

    updated populate table script

--- store/src/scripts/populateDBTables.sql
+++ store/src/scripts/populateDBTables.sql
@@ -1,45 +1,17 @@
-/*
-The following is an example of a "Stored Procedure"
-*/
+INSERT INTO users (username, password, fname,lname,phone, email) VALUES ("alberta1", "gogators", "Alberta", "Gator", "555-666-1234", "alberta1@gmail.com"),
+("albert3", "gogators", "Albert", "Gator", "555-666-1234", "albert3@gmail.com");
+SELECT * FROM users;
 
-INSERT INTO customers (fname, lname, username,email) VALUES ("Dr. P", "Brown", "prbrown", "philippa.brown");
-SELECT * FROM customers;
+INSERT INTO events(title, eventDate, description)
+VALUES
+("Gator Growl", "2019-11-03", "Come show your school spirit at Gator Growl ft. Lil' Baby and Cardi B."),
+("Big Orange", "2019-03-21", "UF's annual comedy show/concert");
+SELECT * FROM events; 
 
-DROP PROCEDURE IF EXISTS insertProducts;
+INSERT INTO tickets(price, sellerId, buyerId, availability, eventId)
+VALUES
+(25, 1, 2, 'Monday 3pm',1),
+(15, 2, 1, 'Friday 8am',2);
+SELECT * FROM tickets;
 
-DELIMITER //
-CREATE PROCEDURE insertProducts
-(IN numProducts INT)
 
-BEGIN
- DECLARE x, upc INT;
- DECLARE regPrice, salePrice DECIMAL(6,2);
- DECLARE prodName, prodType, prodColor, prodSize, descr, brand, gender  VARCHAR(255);
- 
- SET x = 1;
-
- WHILE x  <= numProducts DO
-	 SET prodType = ELT(MOD(x,6)+1, "pants", "shorts", "shirt", "sweater", "sandal", "boot");
-	 SET prodColor = ELT(MOD(x,5)+1, "Blue", "Red", "Black", "Orange", "White");
-	 SET prodSize = ELT(MOD(x,4)+1, "XSmall","Small", "Medium", "Large");
-	 SET descr = ELT(MOD(x,3)+1, "Stylish", "Modern", "Classic");
-	 SET gender = ELT(MOD(x,2)+1, "Female", "Male");
-	 SET brand = "Generic";
-
-	 SET prodName = CONCAT(gender,"'s ",prodType);
-	 SET descr = CONCAT(descr, " ",prodSize," ", prodColor, " ",  prodName); 
-	 SET regPrice = RAND()*(2000-1);
-	 SET salePrice = regPrice * 0.75;
-	 SET upc = FLOOR(RAND()*(999999999-100000000));	 
-
-	 INSERT INTO products (name, msrp, salePrice, upc, shortDescription, brandName, size, color, gender) 
-		VALUES (prodName, regPrice, salePrice, upc, descr, brand, prodSize, prodColor, gender);
-
-	 SET  x = x + 1; 
-
- END WHILE;
- SELECT * FROM products;
- 
-END //
-DELIMITER ;
-CALL insertProducts(20);
