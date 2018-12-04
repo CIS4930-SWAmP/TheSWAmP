a3ea1362c3a2d3c6c4dabf0710d2c900dc9f828d
Althea Smith <asmith790@ufl.edu>
2018-12-04 17:17:14 -0500

    added conditional admin view on event tiles

--- store/src/main/webapp/js/eventTileScript.js
+++ store/src/main/webapp/js/eventTileScript.js
@@ -1,5 +1,13 @@
 var request = new XMLHttpRequest();
 var requestKey = new XMLHttpRequest();
+var isAdmin = false;
+
+//Do login stuff
+if(isAdmin){
+    document.getElementById('sell').style.display = "none";
+    document.getElementById('newEvent').style.display = "block";
+}
+
 
 function searchByKeyword(keyword) {
     requestKey.open('GET', 'http://localhost:8080/TheSWAmP-2.0.3.RELEASE/store/events/search/' + keyword, true);
@@ -51,13 +59,29 @@ function createCards(data){
 
                 const desc = document.createElement('p');
                 desc.textContent = event.desc;
-                desc.setAttribute("style", 'color:#343a40');
+                desc.setAttribute("style", 'color:#343a40;margin-bottom:2rem');
 
                 container.appendChild(card);
                 card.appendChild(h1);
                 card.appendChild(date);
                 card.appendChild(desc);
+
+                if(isAdmin){
+                    var updateBtn = document.createElement('button');
+                    updateBtn.setAttribute('type', 'button');
+                    updateBtn.setAttribute('class', 'btn btn-dark');
+                    updateBtn.setAttribute('style', 'margin:0 0 0 14rem;position:absolute;bottom:1rem;');
+                    updateBtn.setAttribute('class', 'btn btn-dark');
+                    updateBtn.setAttribute('onclick', `updateEvent(event,${event.eventId})`);
+                    updateBtn.innerText = 'Update';
+                    card.appendChild(updateBtn);
+                }
             });
+}
+
+function updateEvent(event, id){
+    event.stopPropagation();
+    window.location = "/TheSWAmP-2.0.3.RELEASE/html/updateEvent.html#"+id;
 }
 
 request.send();
