3f71415d0bd1438ec105bf601647d734a01333ec
Prakash Pudhucode <pudhucodep@gmail.com>
2018-12-05 02:49:46 -0500

    local storage

--- store/src/main/webapp/js/profileScript.js
+++ store/src/main/webapp/js/profileScript.js
@@ -1,92 +1,133 @@
-var request = new XMLHttpRequest();
-var requestUpdate = new XMLHttpRequest();
-var requestHistory = new XMLHttpRequest();
-
-const username = document.cookie.substr(document.cookie.indexOf('=')+1);
-
-request.open('GET', 'http://localhost:8080/TheSWAmP-2.0.3.RELEASE/store/users/'+username, true);
-request.onload = function () {
-    if(request.readyState === 4) {
-        if (request.status === 200) {
-            var data = JSON.parse(this.response);
-            var userInfo = document.getElementById("userInfo");
-
-            var username = document.createElement('h4');
-            username.innerText = 'Username: ' + data.username;
-
-            var email = document.createElement('h4');
-            email.innerText = 'Email: ' + data.email;
-
-            var name = document.createElement('h4');
-            name.innerText = 'Name: ' + data.fname+ ' ' +data.lname;
-
-            var phone = document.createElement('h4');
-            name.innerText = 'Phone: ' + data.phone;
-
-            userInfo.appendChild(username);
-            userInfo.appendChild(email);
-            userInfo.appendChild(name);
-            userInfo.appendChild(phone);
-
-        } else {
-           console.log('Error');
-        }
-    }
-};
-
-function submit() {
-    var fname = document.getElementById('fname').value;
-    var lname = document.getElementById('lname').value;
-    var email = document.getElementById('email').value;
-    var phone = document.getElementById('phone').value;
-
-    var apiUrl = "http://localhost:8080/TheSWAmP-2.0.3.RELEASE/store/users?fname="+fname+"&lname="+lname+
-    "&username="+username+"&phone="+phone+"&email="+email;
-
-    requestUpdate.open('PUT', apiUrl, true );
-    requestUpdate.send();
-    requestUpdate.onreadystatechange = function (){
-        if(requestUpdate.readyState === 4){
-            if(requestUpdate.status === 200){
-                window.location = "http://localhost:8080/TheSWAmP-2.0.3.RELEASE/html/profile.html";
+function updateProfile() {
+    var fname = document.getElementById("fname");
+    var lname = document.getElementById("lname");
+    var username = document.getElementById("username");
+    var password = document.getElementById("password");
+    var phone = document.getElementById("phone");
+    var email = document.getElementById("email");
+    var updateRequest = new XMLHttpRequest();
+    url = 'http://localhost:8080/TheSWAmP-2.0.3.RELEASE/store/users?fname='+fname+'&lname='+lname+'&username='+username+'&password='+password+'&phone='+phone+'&email=' + email;
+    updateRequest.open('PUT', url, true);
+    updateRequest.send();
+    updateRequest.onreadystatechange = function() {
+        if(updateRequest.readyState === 4) {
+            if(updateRequest.status === 200) {
+                alert('Updated information successfully');
             }
-            else{
-                console.log('Error');
+            else {
+                alert('Unable to update information. Please try again later');
             }
         }
     }
 }
 
-requestHistory.open('GET',"http://localhost:8080/TheSWAmP-2.0.3.RELEASE/store/tickets/purchased/"+username, true);
-requestHistory.send();
-requestHistory.onreadystatechange = function (){
-    if(requestHistory.readyState === 4){
-        if(requestHistory.status === 200){
-            var data = JSON.parse(this.response);
-            data.forEach(product => {
-                const history =document.getElementById('history');
-                const div = document.createElement('div');
-                div.setAttribute('class','border border-dark');
-                div.setAttribute('style','padding:5px;max-width:500px;background:#e9ecef');
-
-                const name = document.createElement('p');
-                name.textContent = product.eventId;
-
-
-                const price = document.createElement('p');
-                product.description += product.price;
-                price.textContent = `Sale Price: $${product.price}`;
-
-                div.appendChild(name);
-                div.appendChild(price);
-
-                history.appendChild(div);
-            });
-        }
-        else{
-            console.log('Error');
+function getProfileInfo() {
+    var username = document.getElementById("username");
+    url = 'http://localhost:8080/TheSWAmP-2.0.3.RELEASE/store/users?username='+username;
+    updateRequest.open('GET', url, true);
+    updateRequest.send();
+    updateRequest.onreadystatechange = function() {
+        if(updateRequest.readyState === 4) {
+            if(updateRequest.status === 200) {
+                var res = JSON.parse(verifyRequest.responseText);
+                console.log('get info', JSON.parse(verifyRequest.responseText));
+                document.getElementById("fname").value = res.fname;
+                document.getElementById("lname").value = res.lname;
+                document.getElementById("phone").value = res.phone;
+                document.getElementById("email").value = res.email;
+            }
+            else {
+                alert('Unable to obtain information. Please try again later');
+            }
         }
     }
-};
+}
 
-request.send();
+// const username = document.cookie.substr(document.cookie.indexOf('=')+1);
+//
+// request.open('GET', 'http://localhost:8080/TheSWAmP-2.0.3.RELEASE/store/users/'+username, true);
+// request.onload = function () {
+//     if(request.readyState === 4) {
+//         if (request.status === 200) {
+//             var data = JSON.parse(this.response);
+//             var userInfo = document.getElementById("userInfo");
+//
+//             var username = document.createElement('h4');
+//             username.innerText = 'Username: ' + data.username;
+//
+//             var email = document.createElement('h4');
+//             email.innerText = 'Email: ' + data.email;
+//
+//             var name = document.createElement('h4');
+//             name.innerText = 'Name: ' + data.fname+ ' ' +data.lname;
+//
+//             var phone = document.createElement('h4');
+//             name.innerText = 'Phone: ' + data.phone;
+//
+//             userInfo.appendChild(username);
+//             userInfo.appendChild(email);
+//             userInfo.appendChild(name);
+//             userInfo.appendChild(phone);
+//
+//         } else {
+//            console.log('Error');
+//         }
+//     }
+// };
+//
+// function submit() {
+//     var fname = document.getElementById('fname').value;
+//     var lname = document.getElementById('lname').value;
+//     var email = document.getElementById('email').value;
+//     var phone = document.getElementById('phone').value;
+//
+//     var apiUrl = "http://localhost:8080/TheSWAmP-2.0.3.RELEASE/store/users?fname="+fname+"&lname="+lname+
+//     "&username="+username+"&phone="+phone+"&email="+email;
+//
+//     requestUpdate.open('PUT', apiUrl, true );
+//     requestUpdate.send();
+//     requestUpdate.onreadystatechange = function (){
+//         if(requestUpdate.readyState === 4){
+//             if(requestUpdate.status === 200){
+//                 window.location = "http://localhost:8080/TheSWAmP-2.0.3.RELEASE/html/profile.html";
+//             }
+//             else{
+//                 console.log('Error');
+//             }
+//         }
+//     }
+// }
+//
+// requestHistory.open('GET',"http://localhost:8080/TheSWAmP-2.0.3.RELEASE/store/tickets/purchased/"+username, true);
+// requestHistory.send();
+// requestHistory.onreadystatechange = function (){
+//     if(requestHistory.readyState === 4){
+//         if(requestHistory.status === 200){
+//             var data = JSON.parse(this.response);
+//             data.forEach(product => {
+//                 const history =document.getElementById('history');
+//                 const div = document.createElement('div');
+//                 div.setAttribute('class','border border-dark');
+//                 div.setAttribute('style','padding:5px;max-width:500px;background:#e9ecef');
+//
+//                 const name = document.createElement('p');
+//                 name.textContent = product.eventId;
+//
+//
+//                 const price = document.createElement('p');
+//                 product.description += product.price;
+//                 price.textContent = `Sale Price: $${product.price}`;
+//
+//                 div.appendChild(name);
+//                 div.appendChild(price);
+//
+//                 history.appendChild(div);
+//             });
+//         }
+//         else{
+//             console.log('Error');
+//         }
+//     }
+// };
+//
+// request.send();
